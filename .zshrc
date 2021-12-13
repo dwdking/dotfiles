617c9039a714b36dc4184b8d9b4395f61f403581
Dan King <dan@dwdking.ca>
2021-12-12 20:38:16 -0500

    Adding oh-my-zsh and custom config

--- .zshrc
+++ .zshrc
@@ -1,7 +1,10 @@
 # defaults loaded from /etc/zsh/zshrc. You can clobber this file with your own
 # from a dotfiles repo to override.
-source /etc/zsh/zshrc.default.inc.zsh
 
-alias g="git"
-alias ll="ls -l"
-alias la="ls -al"
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="dwdking"
+ZSH_CUSTOM="$HOME/dotfiles/omz-custom"
+
+plugins=(git colored-man-pages colorize)
+
+source $ZSH/oh-my-zsh.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
@@ -7,6 +9,7 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+    --exclude "omz-custom" \
 		-avh --no-perms . ~/;
 	source ~/.zshrc;
 }
