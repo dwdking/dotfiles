d0e6d7bce48da841f32b7940797de20d92ec79e8
Dan King <dan@dwdking.ca>
2022-02-01 21:14:04 -0500

    Fixing up bootstrap and install scripts before adding submodule

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env zsh
 
-# Find safer way
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+    	--exclude "omz-custom" \
+		--exclude "ohmyzsh" \
+		-avh --no-perms . ~/;
+	source ~/.zshrc;
+}
 
-# function doIt() {
-# 	rsync --exclude ".git/" \
-# 		--exclude ".DS_Store" \
-# 		--exclude ".osx" \
-# 		--exclude "bootstrap.sh" \
-# 		--exclude "README.md" \
-# 		--exclude "LICENSE-MIT.txt" \
-#     --exclude "omz-custom" \
-# 		-avh --no-perms . ~/;
-# 	source ~/.zshrc;
-# }
-
-# doIt;
-# unset doIt;
+doIt;
+unset doIt;
