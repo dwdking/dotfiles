6e90b011151421cc57201339d266cce4db657cb3
Dan King <dan@dwdking.ca>
2022-04-22 20:33:11 -0400

    Adding better ssh completions

--- omz-custom/config.zsh
+++ omz-custom/config.zsh
@@ -1,3 +1,6 @@
 GPG_TTY=$(tty)
 export GPG_TTY
-KEYID=0x8CCF4D624B38951C
+KEYID=0x8CCF4D624B38951Ca
+
+zstyle ':completion:*:(ssh|scp|ftp|sftp):*' hosts $hosts
+zstyle ':completion:*:(ssh|scp|ftp|sftp):*' users $users
