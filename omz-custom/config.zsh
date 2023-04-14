1417cf94b0025ca575ed482f2e94109f41186c1b
Dan King <danwd.king@shopify.com>
2023-04-14 10:01:10 -0400

    Fixing id

--- omz-custom/config.zsh
+++ omz-custom/config.zsh
@@ -1,6 +1,6 @@
 GPG_TTY=$(tty)
 export GPG_TTY
-KEYID=0x8CCF4D624B38951Ca
+KEYID=0x8CCF4D624B38951C
 
 zstyle ':completion:*:(ssh|scp|ftp|sftp):*' hosts $hosts
 zstyle ':completion:*:(ssh|scp|ftp|sftp):*' users $users
